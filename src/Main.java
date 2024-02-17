a262e29e646cfe3863550b18c6fe30ab26e0404f
Nikita Mihailuk <mihnik.9898@gmail.com>
2024-02-17 17:51:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите уравнение  вида x +-/* a = b:");
+        String input = in.nextLine();
+
+        char operator = ' ';
+        double a = 0;
+        double b = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char n = input.charAt(i);
+            if (n == '+' || n == '-' || n == '/' || n == '*') {
+                operator = n;
+            }
+            else if (Character.isDigit(n)) {
+                if (input.charAt(i-2) != '=' ) {
+                    a = Character.getNumericValue(n);
+                }
+                else {
+                    b = Character.getNumericValue(n);
+                }
+            }
+        }
+        if (operator == '+') System.out.println("x = " + (b - a));
+        else if (operator == '-') System.out.println("x = " + (b + a));
+        else if (operator == '/') System.out.println("x = " + (b * a));
+        else if (operator == '*') System.out.println("x = " + (b / a));
+        else System.out.println("Неправильно введённое уравнение");
+
+    }
+}
